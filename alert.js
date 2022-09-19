e8bfa8819cd1a594a6cdef0a7722da4f63886591
nasirmondol <nasirahmed8202@gmail.com>
2022-09-18 22:28:03 -0700

    html and js commit

---
+++ alert.js
@@ -0,0 +1,17 @@
+console.log('I am here.')
+// alert("sir is coming")
+
+const sirCOming = () =>{
+    alert('Be careful, sir is coming!!')
+}
+
+const askPicnic =() =>{
+    const response = confirm('Are you going to picnic?')
+    console.log(response)
+    if(response === true){
+        alert('amke taka bkash kore de')
+    }
+    else{
+        console.log('dure giye morga!!')
+    }
+}
