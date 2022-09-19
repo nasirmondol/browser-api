30ac271ebf18e4106b3b299409af108103f32fff
nasirmondol <nasirahmed8202@gmail.com>
2022-09-18 22:01:42 -0700

    second commit

---
+++ explore.js
@@ -0,0 +1,12 @@
+const name = 'Jabed';
+
+const student = {
+    name: 'sagor', 
+    id: 101,
+    friend: 'rajib',
+    makeFriend: function(name){
+        console.log(name)
+    }
+}
+student.makeFriend('sagor chowdhury')
+// document.getElementById('name')
